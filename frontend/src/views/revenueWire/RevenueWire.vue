0a2621303c7ca01a233b81056690c3566eb1ef09
Edin Mesanovic <edinmesanovic@DESKTOP-66SNTUL.localdomain>
2021-02-01 08:54:09 +0100

    restored changes

--- frontend/src/views/revenueWire/RevenueWire.vue
+++ frontend/src/views/revenueWire/RevenueWire.vue
@@ -26,19 +26,19 @@
       <div class="col-4">
         <h5>Inkluder</h5>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" v-model="includeFirst">
+          <input class="form-check-input" type="checkbox" v-model="listOfBools.includeFirst">
           <label class="form-check-label">
             Inkluder første liste
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" v-model="includeSecond">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.includeSecond">
           <label class="form-check-label">
             Inkluder anden liste
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" v-model="includeThird">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.includeThird">
           <label class="form-check-label">
             Inkluder tredje liste
           </label>
@@ -54,27 +54,29 @@
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.wrapWithQuotes">
           <label class="form-check-label">
             Wrap med quotes: " "
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.wrapWithBrackets">
           <label class="form-check-label">
             Wrap med brackets: []
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
-          <label class="form-check-label">
-            Wrap med:
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.wrapWithSymbol">
+          <label class="form-check-label">Wrap med symboler: <input type="text" style="width: 50px" ref="wrapSymbol"
+                                                                    placeholder="-"> <input
+              type="text" style="width: 50px" ref="wrapSymbol2" placeholder="+">
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
+        <div class="form-check mt-1">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.wrapWithWords">
           <label class="form-check-label">
-            Wrap med:
+            Wrap med ord: <input type="text" style="width: 50px" ref="wrapWord" placeholder="køb"> <input
+              type="text" style="width: 50px" ref="wrapWord2" placeholder="nu">
           </label>
         </div>
       </div>
@@ -82,13 +84,13 @@
       <div class="col-4">
         <h5>Scrub</h5>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.removeDupli">
           <label class="form-check-label">
             Fjern duplikater
           </label>
         </div>
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="">
+          <input class="form-check-input" type="checkbox" value="" v-model="listOfBools.removeExtraSpa">
           <label class="form-check-label">
             Fjern ekstra spaces
           </label>
@@ -96,7 +98,7 @@
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="">
           <label class="form-check-label">
-            Fjern karakterne: INPUT FELT
+            Fjern tegn: INPUT FELT
           </label>
         </div>
         <div class="form-check">
@@ -126,7 +128,8 @@
       </div>
     </div>
 
-    <button type="button" class="btn btn-success" v-on:click="combineList()">Generer resultat</button>
+    <button type="button" class="btn btn-success" v-on:click="combine()">Generer resultat</button>
+    <button type="button" class="btn btn-warning ml-2" v-on:click="clearOptions()">Nulstil valg</button>
 
     <!-- Text area showing the result -->
     <div class="row">
@@ -139,7 +142,7 @@
     </div>
     <div class="row">
       <div class="col-12">
-        <button type="button" class="btn btn-success">Gem i databasen</button>
+        <button type="button" class="btn btn-success" v-on:click="">Gem i databasen</button>
         <button type="button" class="btn btn-warning ml-2" id="clearbtn" v-on:click="clearAll()">Tøm listen</button>
       </div>
     </div>
@@ -155,66 +158,124 @@ export default {
   data() {
     return {
       combinedList: [],
-      list: [],
-      includeFirst: true,
-      includeSecond: true,
-      includeThird: true
+      list1: [],
+      list2: [],
+      list3: [],
+      listOfBools: [{
+        includeFirst: true
+      },
+        {
+          includeSecond: true
+        },
+        {
+          includeThird: true
+        },
+        {
+          wrapWithQuotes: false
+        },
+        {
+          wrapWithBrackets: false
+        },
+        {
+          wrapWithSymbol: false
+        },
+        {
+          wrapWithWords: false
+        },
+        {
+          removeDupli: false
+        },
+        {
+          removeExtraSpa: false
+        }]
     }
   },
   computed: {},
   methods: {
-    combineList() {
+    getValuesFromTextAreas() {
+      // gets all values from textarea1, 2 and 3
+      this.list1 = this.$refs.textarea1.value.split("\n");
+      this.list2 = this.$refs.textarea2.value.split("\n");
+      this.list3 = this.$refs.textarea3.value.split("\n");
+    },
+    populateCombinedList(...args) {
+      var dict = {};
+      dict['includeAll'] = function (...args) {
+        console.log('includeAll')
+
+        let l1l2combined = []
+        let finalList = []
+
+        let val;
+        for (const key in args[2]) {
+          val = args[2][key]
+          for (const key in args[3]) {
+            l1l2combined.push(val + ' ' + args[3][key])
+          }
+        }
+        for (const key in l1l2combined) {
+          val = l1l2combined[key]
+          for (const key in args[4]) {
+            finalList.push(val + ' ' + args[4][key])
+          }
+        }
+        for (const key in finalList) {
+          args[1].push(finalList[key])
+        }
+      }
+      dict['includeTwo'] = function (...args) {
+        console.log('includeFirstAndThird')
+
+        let finalList = []
+        let val
+        for (const key in args[2]) {
+          val = args[2][key]
+          for (const key in args[3]) {
+            finalList.push(val + ' ' + args[3][key])
+          }
+        }
+
+        for (const key in finalList) {
+
+          args[1].push(finalList[key])
+        }
+      }
+      dict[args[0]](...args)
+
+    },
+    combine() {
+      this.emptyCombinedList()
+
+      this.getValuesFromTextAreas()
+
+      // empty the resultarea
       this.$refs.resultarea.value = '';
-      if (this.includeFirst && this.includeSecond && this.includeThird) {
-        this.includeAll()
-      } else if (this.includeFirst && this.includeThird) {
-        this.includeFirstAndThird()
-      } else if (this.includeFirst && this.includeSecond) {
-        this.includeFirstAndSecond()
+
+      // checks which checkboxes are checked and populate the combinedList
+      if (this.listOfBools.includeFirst && this.listOfBools.includeSecond && this.listOfBools.includeThird) {
+        this.populateCombinedList('includeAll', this.combinedList, this.list1, this.list2, this.list3)
+      } else if (this.listOfBools.includeFirst && this.listOfBools.includeThird) {
+        this.populateCombinedList('includeTwo', this.combinedList, this.list1, this.list3)
+      } else if (this.listOfBools.includeSecond && this.listOfBools.includeThird) {
+        this.populateCombinedList('includeTwo', this.combinedList, this.list2, this.list3)
+      } else if (this.listOfBools.includeFirst && this.listOfBools.includeSecond) {
+        this.populateCombinedList('includeTwo', this.combinedList, this.list1, this.list2)
       }
+
+      this.loopThroughCheckboxes()
     },
-    includeFirstAndSecond() {
-      // get values from text areas and split the values by \n
-      let list1 = this.$refs.textarea1.value.split("\n");
-      let list2 = this.$refs.textarea2.value.split("\n");
-
-      // empty combinedList
-      this.combinedList = []
-
-      // combine the values from list1 with list2 and 3. Push the new values into combinedList
-      for (const key in list1) {
-        this.combinedList.push(list1[key] + ' ' + list2[key] + '\n')
+    clearOptions() {
+      for (const key in this.listOfBools) {
+        this.listOfBools[key] = false
       }
-      this.showCombinedList()
+      this.$refs.wrapSymbol.value = '';
+      this.$refs.wrapSymbol2.value = '';
+      this.$refs.wrapWord.value = '';
+      this.$refs.wrapWord2.value = '';
     },
-    includeFirstAndThird() {
-      // get values from text areas and split the values by \n
-      let list1 = this.$refs.textarea1.value.split("\n");
-      let list3 = this.$refs.textarea3.value.split("\n");
-
-      // empty combinedList
+    emptyCombinedList() {
+      // empties the combinedList
       this.combinedList = []
-
-      // combine the values from list1 with list2 and 3. Push the new values into combinedList
-      for (const key in list1) {
-        this.combinedList.push(list1[key] + ' ' + list3[key] + '\n')
-      }
-      this.showCombinedList()
-    },
-    includeAll() {
-      // get values from text areas and split the values by \n
-      let list1 = this.$refs.textarea1.value.split("\n");
-      let list2 = this.$refs.textarea2.value.split("\n");
-      let list3 = this.$refs.textarea3.value.split("\n");
-
-      // empty combinedList
-      this.combinedList = []
-
-      // combine the values from list1 with list2 and 3. Push the new values into combinedList
-      for (const key in list1) {
-        this.combinedList.push(list1[key] + ' ' + list2[key] + ' ' + list3[key] + '\n')
-      }
-      this.showCombinedList()
     },
     clearAll() {
       // clears everything
@@ -222,18 +283,97 @@ export default {
       let list1 = this.$refs.textarea1.value = '';
       let list2 = this.$refs.textarea2.value = '';
       let list3 = this.$refs.textarea3.value = '';
-    }
-    ,
+    },
     showCombinedList() {
+      // sets the value of resultarea to combinedList
+      let newList = new Array()
+      for (const key in this.combinedList) {
+        newList.push(this.combinedList[key] + '\n')
+      }
+      this.combinedList = newList
+
       this.$refs.resultarea.value = this.combinedList.join("")
+    },
+    wrapTxtWithQuotes() {
+      let newList = new Array();
+      for (const key in this.combinedList) {
+        const val = this.combinedList[key]
+        newList.push('"' + val + '"')
+      }
+      this.combinedList = newList
+    },
+    wrapTxtWithBrackets() {
+      let newList = new Array();
+      for (const key in this.combinedList) {
+        const val = this.combinedList[key]
+        newList.push('[' + val + ']')
+      }
+      this.combinedList = newList
+    },
+    wrapTxtWithInput() {
+      let newList = new Array();
+      let wrapSymbol = this.$refs.wrapSymbol.value
+      let wrapSymbol2 = this.$refs.wrapSymbol2.value
+
+      for (const key in this.combinedList) {
+        const val = this.combinedList[key]
+        newList.push(wrapSymbol + val + wrapSymbol2)
+      }
+      this.combinedList = newList
+    },
+    wrapTxtWithWords() {
+      let newList = []
+      let wrapWord = this.$refs.wrapWord.value
+      let wrapWord2 = this.$refs.wrapWord2.value
+
+      for (const key in this.combinedList) {
+        const val = this.combinedList[key]
+        newList.push(wrapWord + ' ' + val + ' ' + wrapWord2)
+      }
+      this.combinedList = newList
+    },
+    removeDuplicates() {
+      let unique = [...new Set(this.combinedList)];
+      this.combinedList = unique
+    },
+    removeExtraSpaces() {
+      let newList = []
+      for (const key in this.combinedList) {
+        let val = this.combinedList[key]
+        val.replace(/\s+/g, ' ').trim()
+        let newString = val.replace(/\s+/g, ' ').trim()
+        newList.push(newString)
+      }
+      this.combinedList = newList
+    },
+    loopThroughCheckboxes() {
+      if (this.listOfBools.wrapWithWords) {
+        this.wrapTxtWithWords()
+      }
+      if (this.listOfBools.removeExtraSpa) {
+        this.removeExtraSpaces()
+      }
+      if (this.listOfBools.removeDupli) {
+        this.removeDuplicates()
+      }
+      if (this.listOfBools.wrapWithQuotes) {
+        this.wrapTxtWithQuotes()
+      }
+      if (this.listOfBools.wrapWithBrackets) {
+        this.wrapTxtWithBrackets()
+      }
+      if (this.listOfBools.wrapWithSymbol) {
+        this.wrapTxtWithInput()
+      }
+      this.showCombinedList()
     }
+
   }
 }
 </script>
 
 <style scoped>
-#clearbtn {
+.btn-warning {
   color: white;
 }
 </style>
-
