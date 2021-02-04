bbf74041069bb36efbc803e7d8e3ccb4e4287d5d
Edin Mesanovic <edinmesanovic@DESKTOP-66SNTUL.localdomain>
2021-02-04 11:33:28 +0100

    Ændringer i styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,6 +9,22 @@ export default {
 </script>
 
 <style lang="scss">
-  // Import Main styles for this application
-  @import 'assets/scss/style';
+// Import Main styles for this application
+@import 'assets/scss/style';
+
+@font-face {
+  font-family: "Sofia Pro Bold";
+  src: local("Sofia Pro Bold"),
+  url('./fonts/Mostardesign - Sofia Pro Bold.otf') format("truetype");
+}
+@font-face {
+  font-family: "Sofia Pro Regular";
+  src: local("Sofia Pro Regular"),
+  url('./fonts/Mostardesign - Sofia Pro Bold.otf') format("truetype");
+}
+@font-face {
+  font-family: "Sofia Pro Light";
+  src: local("Sofia Pro Light"),
+  url('./fonts/Mostardesign - Sofia Pro Light.otf') format("truetype");
+}
 </style>
