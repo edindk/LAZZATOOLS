47029f08d6899cba7483203c0e040fe897a33da2
edindk <edindkk@gmail.com>
2021-03-11 13:56:42 +0100

    ..

--- frontend/src/containers/TheSidebar.vue
+++ frontend/src/containers/TheSidebar.vue
@@ -9,8 +9,8 @@
       <img src="../assets/logo/lazzaweb-logo-dark.svg" width="220px">
     </CSidebarBrand>
 
-    <CRenderFunction v-if="this.showFullSidebar" flat :content-to-render="$options.nav"/>
-    <CRenderFunction v-else flat :content-to-render="$options.navGuest"/>
+    <CRenderFunction flat :content-to-render="$options.nav"/>
+
     <CSidebarMinimizer id="sidebarFooter"
                        class="d-md-down-none"
                        @click.native="$store.commit('set', ['sidebarMinimize', !minimize])"
@@ -20,23 +20,12 @@
 
 <script>
 import nav from './_nav'
-import navGuest from './_navGuest'
 
 export default {
   name: 'TheSidebar',
   nav,
-  navGuest,
-  data() {
-    return {
-      showFullSidebar: false
-    }
-  },
   computed: {
     show() {
-      if (this.$store.getters.userDetails.email.split('@')[1] === 'lazzaweb.dk' && this.$store.getters.userDetails.email_verified_at) {
-        this.showFullSidebar = true
-      }
-
       return this.$store.state.sidebarShow
     },
     minimize() {
