e1a0547fd7930f64df214bab0be64d51b812f031
edindk <edindkk@gmail.com>
2021-03-18 10:13:49 +0100

    Tilføjet model klassen Search

---
+++ Backend/app/Search.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Search extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'searchWord', 'status'
+    ];
+}
